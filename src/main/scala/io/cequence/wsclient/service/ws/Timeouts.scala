d7ed2fb5596c47e62a09510ba217e55b5b0a9d69
Boris Burdiliak <boris.burdiliak@gmail.com>
2024-05-22 16:27:15 +0200

    timeouts' isDefined and toOption functions

--- src/main/scala/io/cequence/wsclient/service/ws/Timeouts.scala
+++ src/main/scala/io/cequence/wsclient/service/ws/Timeouts.scala
@@ -5,4 +5,10 @@ case class Timeouts(
   readTimeout: Option[Int] = None,
   connectTimeout: Option[Int] = None,
   pooledConnectionIdleTimeout: Option[Int] = None
-)
+) { self =>
+  def isDefined: Boolean =
+    requestTimeout.isDefined || readTimeout.isDefined || connectTimeout.isDefined || pooledConnectionIdleTimeout.isDefined
+
+  def toOption: Option[Timeouts] =
+    if (isDefined) Some(self) else None
+}
